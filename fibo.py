4f111e7db325a2b5b37c4b41bee4a5c213c5a433
Honzik17 <jan1pecha@gmail.com>
2025-04-28 09:33:56 +0200

    Fico

--- fibo.py
+++ fibo.py
@@ -1,5 +1,17 @@
+def recursive_nth_fibo(poradi):
+    if poradi == 0:
+        return 0
+    elif poradi == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(poradi - 1) + recursive_nth_fibo(poradi - 2)
 
 def main():
+    cislo = input("Napiš kolik chceš čísel bro ")
+    emoji = [0, 1]
+    for i in range(2, int(cislo)+1):
+        emoji.append(recursive_nth_fibo(i))
+    print(emoji)
     pass
 
 
